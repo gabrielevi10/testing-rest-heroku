804896f5ac742d6efd39102dd9acb2e12f33e348
gabrielevi10 <gabrielevigomes@gmail.com>
2018-05-17 22:30:43 -0300

    b

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from flask import Flask
+import json
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
+@app.route('/', methods = ['GET'])
+def home():
     return "Hello World."
 
+port = int(os.environ.get('PORT', 5000))
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
